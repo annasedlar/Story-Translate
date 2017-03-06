26477e1f3d4a7c307c403825c0ee011a50cc379c
Anna Sedlar <annasedlar@gmail.com>
2017-03-06 10:49:19 -0500

    embed video

--- frontend/index.js
+++ frontend/index.js
@@ -40,4 +40,5 @@ app.controller('helloController',['$scope', function($scope){
 
 app.controller('aboutController',['$scope', function($scope){
     $scope.about = "ABOUT";
+    $scope.videoURL = '<iframe src="https://www.youtube.com/embed/PeonBmeFR8o?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>'
 }]);
