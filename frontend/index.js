4d80f9d7343b4e3f8214708e8f76f37c4a427aa1
Anna Sedlar <annasedlar@gmail.com>
2017-03-06 10:31:14 -0500

    routing working

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,15 @@
-var app = angular.module('app', ['ui-router']);
+var app = angular.module('app', ['ui.router']);
 
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
     // give name to state. in this case, it is 'firstMessage'
     // as a part of the stateProvider, you need to provide a new state by doing $stateProvider.state
+    $stateProvider.state('home',{
+        url:'/',
+        templateUrl: 'home.html', 
+        controller: 'homeController'
+
+    })
     $stateProvider.state('hello',{
     
         // provide options for state in between the {}
@@ -12,27 +18,26 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         url:'/hello',
         // template:'<strong>This is the first message</strong>'
         templateUrl:'hello.html',
-        controller: 'hello'
+        controller: 'helloController'
     })
     // console.log('hello')
     $stateProvider.state('about',{
         url:'/about',
         templateUrl:'about.html',
-        controller: 'about'
+        controller: 'aboutController'
     })
-    // .state('root',{
-    //     url:'/',
-    //     template: '<strong>You are at the root route</strong>'
-    // });
 
     $urlRouterProvider.otherwise('/');
 }]);
 
+app.controller('homeController',['$scope', function($scope){
+    $scope.home = "HOME";
+}]);
 
-app.controller('hello',['$scope', function($scope){
+app.controller('helloController',['$scope', function($scope){
     $scope.hello = "HI"
 }]);
 
-app.controller('about',['$scope', function($scope){
+app.controller('aboutController',['$scope', function($scope){
     $scope.about = "ABOUT";
 }]);
