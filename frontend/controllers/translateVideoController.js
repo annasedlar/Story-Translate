b1bf34ceb819f709b1dfe2075458ec5edd5e1d93
Paul <dwkang@uga.edu>
2017-03-06 14:27:29 -0500

    translation page continued

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -2,11 +2,24 @@
 app.controller('translateVideoController',['$scope', function($scope){
 	$scope.paused='0'
     $scope.submitUrl = function() {
-		$scope.videoUrl = $('#target-video').html('<iframe src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
+		// $scope.videoUrlInput= 'https://www.youtube.com/embed/PeonBmeFR8o?ecver=2'
+
+
+		$scope.videoUrl = $('#target-video').html('<iframe id="videoID" src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
     };
+
+
+
+
+
+
+
     $scope.pausedFunction = function(){
-    	var pausedTime = document.getElementsByTagName("video")[0].currentTime
+    	var pausedTime = $("video")[0].currentTime
     	$scope.paused = $('#paused').html('You paused at' + pausedTime)
+    	console.log(pausedTime)
     }
 }]);
 
+
+// iframe.html.body
