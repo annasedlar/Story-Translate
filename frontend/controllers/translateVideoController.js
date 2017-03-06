4c2452c20cad9bebaf271db8e561f1f91079d041
Anna Sedlar <annasedlar@gmail.com>
2017-03-06 17:04:23 -0500

    translate page form

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -1,25 +1,33 @@
 
 app.controller('translateVideoController',['$scope', function($scope){
-	$scope.paused='0'
-    $scope.submitUrl = function() {
-		// $scope.videoUrlInput= 'https://www.youtube.com/embed/PeonBmeFR8o?ecver=2'
+  $scope.paused='0'
+  $scope.length = '0'
+  $scope.startTime = '0'
+  $scope.endTime = '0'
+  //   $scope.submitUrl = function() {
+    // $scope.videoUrl = $('#target-video').html('<iframe src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
+  //   };
+  //   $scope.pausedFunction = function(){
+  //    var pausedTime = document.getElementsByTagName("video")[0].currentTime
+  //    $scope.paused = $('#paused').html('You paused at' + pausedTime)
+  //   }
+      $scope.startTimeFunc = function(){
+        var theVid = document.getElementById("theVid")
+        console.dir(theVid);
+        $scope.startTime =theVid.currentTime
+        console.log(startTime)
+        // $scope.paused = pausedTime
+        // $scope.length = vLength
 
+      }
+      $scope.endTimeFunc = function(){
+        var theVid = document.getElementById("theVid")
+        console.dir(theVid);
+        $scope.endTime = theVid.currentTime
+        console.log(endTime)
+        // $scope.paused = pausedTime
+        // $scope.length = vLength
 
-		$scope.videoUrl = $('#target-video').html('<iframe id="videoID" src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
-    };
-
-
-
-
-
-
-
-    $scope.pausedFunction = function(){
-    	var pausedTime = $("video")[0].currentTime
-    	$scope.paused = $('#paused').html('You paused at' + pausedTime)
-    	console.log(pausedTime)
-    }
+      }
 }]);
 
-
-// iframe.html.body
