2d6d60478e49587f2771a9412010cb9544c52331
Anna Sedlar <annasedlar@gmail.com>
2017-03-07 11:14:38 -0500

    trying to get form to push to right side angular

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -4,6 +4,7 @@ app.controller('translateVideoController',['$scope', function($scope){
   $scope.length = '0'
   $scope.startTime = '0'
   $scope.endTime = '0'
+  $scope.entireTranscript = []
   //   $scope.submitUrl = function() {
     // $scope.videoUrl = $('#target-video').html('<iframe src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
   //   };
@@ -11,6 +12,18 @@ app.controller('translateVideoController',['$scope', function($scope){
   //    var pausedTime = document.getElementsByTagName("video")[0].currentTime
   //    $scope.paused = $('#paused').html('You paused at' + pausedTime)
   //   }
+      $scope.submitEachSection = function(){
+        var startTime = $scope.startTime;
+        var endTime = $scope.endTime; 
+        var transcript = $scope.transcript; 
+        $scope.entireTranscript.push({
+          startTime: startTime,
+          endTime: endTime,
+          transcript: transcript
+        })
+        console.log($scope.entireTranscript)
+      }
+
       $scope.startTimeFunc = function(translateNumber){
         console.log(translateNumber)
         var theVid = document.getElementById("theVid")
