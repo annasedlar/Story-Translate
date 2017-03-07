59ed5682d654e0922c5db3f6d8ff4d068ebf8a30
Anna Sedlar <annasedlar@gmail.com>
2017-03-07 11:48:57 -0500

    trying to get form working

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -4,6 +4,7 @@ app.controller('translateVideoController',['$scope', function($scope){
   $scope.length = '0'
   $scope.startTime = '0'
   $scope.endTime = '0'
+  $scope.transcript =''
   $scope.entireTranscript = []
   //   $scope.submitUrl = function() {
     // $scope.videoUrl = $('#target-video').html('<iframe src=' + $scope.videoUrlInput + ' width="640" height="360" frameborder="0" style="position:block;width:50vw;height:50vh;left:0" allowfullscreen></iframe>')
@@ -28,7 +29,7 @@ app.controller('translateVideoController',['$scope', function($scope){
         console.log(translateNumber)
         var theVid = document.getElementById("theVid")
         console.dir(theVid);
-        $scope.startTime =theVid.currentTime
+        $scope.startTime = theVid.currentTime
         console.log($scope.startTime)
         // $scope.paused = pausedTime
         // $scope.length = vLength
@@ -45,13 +46,11 @@ app.controller('translateVideoController',['$scope', function($scope){
       }
 
       $scope.addField = function(){
-        $(document).ready(function(){
           var newField = '<div><button class="btn btn-info" ng-click="startTimeFunc(1)">Set Start Time</button><br/>Start Time: {{startTime}}<br/><button class="btn btn-info" ng-click="endTimeFunc(1)">Set End Time</button><br/>End Time: {{endTime}}<br />Enter Translations: <TEXTAREA NAME="Address" ROWS=10 COLS=90></TEXTAREA><input type="submit" style="display:none"/></div>' 
           var x = 1; 
           $("#addFields").click(function(){
             $('#translation-form').append(newField)
           })
-        })
       }
 
       $scope.submitForm = function(){
