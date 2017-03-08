90da0aef8473e06fd02e053ff0a3bb3fd9648a4a
Anna Sedlar <annasedlar@gmail.com>
2017-03-08 12:22:50 -0500

    add thumbnail on upload

--- frontend/controllers/uploadRawVideoController.js
+++ frontend/controllers/uploadRawVideoController.js
@@ -1,5 +1,58 @@
 app.controller('uploadRawVideoController',function($scope, $location, $http){
 	$scope.product = {};
+	 // this gets thumbanil pic of submission
+	$scope.thumbnail = document.getElementById('file').addEventListener('change', function(event) {
+			  var file = event.target.files[0];
+			  var fileReader = new FileReader();
+			  if (file.type.match('image')) {
+			    fileReader.onload = function() {
+			      var img = document.createElement('img');
+			      img.src = fileReader.result;
+			      document.getElementsByTagName('div')[0].appendChild(img);
+			    };
+			    fileReader.readAsDataURL(file);
+			  } else {
+			    fileReader.onload = function() {
+			      var blob = new Blob([fileReader.result], {type: file.type});
+			      var url = URL.createObjectURL(blob);
+			      var video = document.createElement('video');
+			      var timeupdate = function() {
+			        if (snapImage()) {
+			          video.removeEventListener('timeupdate', timeupdate);
+			          video.pause();
+			        }
+			      };
+			      video.addEventListener('loadeddata', function() {
+			        if (snapImage()) {
+			          video.removeEventListener('timeupdate', timeupdate);
+			        }
+			      });
+			      var snapImage = function() {
+			        var canvas = document.createElement('canvas');
+			        canvas.width = video.videoWidth;
+			        canvas.height = video.videoHeight;
+			        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+			        var image = canvas.toDataURL();
+			        var success = image.length > 100000;
+			        if (success) {
+			          var img = document.createElement('img');
+			          img.src = image;
+			          document.getElementsByTagName('div')[0].appendChild(img);
+			          URL.revokeObjectURL(url);
+			        }
+			        return success;
+			      };
+			      video.addEventListener('timeupdate', timeupdate);
+			      video.preload = 'metadata';
+			      video.src = url;
+			      // Load video in Safari / IE11
+			      video.muted = true;
+			      video.playsInline = true;
+			      video.play();
+			    };
+			    fileReader.readAsArrayBuffer(file);
+			  }
+			});
 
 	$scope.submit = function(){
 		var formData = new FormData;
@@ -9,7 +62,6 @@ app.controller('uploadRawVideoController',function($scope, $location, $http){
 		// image file
 		var file = $('#file')[0].files[0];
 		formData.append('video', file);
-
 		// post 
 		$http.post('http://localhost:3000/videos', formData, {
 			transformRequest: angular.identity,
@@ -18,7 +70,7 @@ app.controller('uploadRawVideoController',function($scope, $location, $http){
 			}
 		}).then(
       function successFunction(data){
-        // console.log(data)
+        console.log(data)
         $location.path('/videosToTranslate')
       },
       function failedFunction(data){
@@ -28,3 +80,4 @@ app.controller('uploadRawVideoController',function($scope, $location, $http){
     )
 	}
 });
+// console.dir(document.getElementsByTagName('input'))
