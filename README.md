8f252f305352f9e3c81d7938bbd9cea41a1c1f10
Connie Dang <conniedang576@gmail.com>
2017-03-14 16:49:37 -0400

    started readme

---
+++ README.md
@@ -0,0 +1,147 @@
+#Title of Project
+New Story Group Video Translation Tool
+
+##Description/Overview
+This app allows groups to upload, view, and transcribe/translate videos in an organized way.
+
+
+[Live URL]()
+
+##Table of contents
+
+[Description/Overview](#description/overview)
+
+[Technologies Used](#technologies)
+
+[Challenges and Solutions](#challenges-and-solutions)
+
+[GitHub Link](#github-link)
+
+[Code Snippets and Screenshots](#code-snippets-and-screenshots)
+
+[MVP](#mvp-minimal-viable-product)
+
+[Stretch Goals](#stretch-goals)
+
+[Team Members and Contributions](#team-members-and-contributions)
+
+[Project Timeline](#project-timeline)
+
+
+
+##Technologies
+The following languages and frameworks were used:
+
+  * HTML
+
+  * CSS
+
+  * SASS
+
+  * Javascript
+
+  * Angular 1.5
+
+  * Bootstrap
+
+  * Materialize
+
+  * Node.js
+
+  * MySQL
+
+
+##Challenges and Solutions
+1. **Challenge: ** Learning and utilizing Angular to complete this projectå
+
+   **Solution: **
+
+2. **Challenge: ** Using the video element in HTML5
+
+   **Solution: ** SCE in Angular, grabbing screenshots
+
+3. **Challenge: **Backend routing
+
+   **Solution: **
+
+4. **Challenge: **Token authentication Login
+
+   **Solution: **
+
+##GitHub Link
+[Upd8ed](https://github.com/annasedlar/Story-Translate)
+
+##Code Snippets and Screenshots
+
+
+
+![alt]()
+
+
+
+```js
+
+code snippet here
+
+```
+
+
+##MVP (Minimal Viable Product)
+
+**Ability to upload content
+
+**Submit translations based on video time frames
+
+**Form that saves to database
+
+**Display a list of videos sorted by family and categorized by completion status
+
+
+
+
+##Stretch Goals
+
+**Token authentication system
+
+**Displaying completed translations from transcipt as video plays
+
+##Team Members and Contributions
+All team members are full stack web development students of the [DigitalCrafts](http://www.digitalcrafts.com/) November 2016 cohort. This project utilizes our frontend and backend skills that we have learned along with pair programming and SCRUM agile development methodology. 
+
+
+
+* [Paul Kang](https://github.com/pdwkang) 
+
+**Role:** MVP
+
+**Contributions:** Styling. functionality. lead developer. Resolved coding challenges team members faced throughout the project.
+
+
+* [Anna Sedlar](https://github.com/annasedlar) 
+
+**Role:** SCRUM Master
+
+**Contributions:** translation page, ideas for routes, styling, communicated with Morgan, Readme
+
+
+* [Connie Dang](https://github.com/dangconnie) 
+
+**Role:** Wordsmith
+
+**Contributions:** Readme, upload video component, styling
+
+
+
+
+**I want to help out!**
+
+*If you would like to report an issue with our code or a suggest a way to improve it, simply contact one of us. We are always looking for ways to improve!* 
+
+
+If you would like to contribute to our project, we are looking to add the the following:
+
+
+##Project Timeline
+Project start: 3/6/2017
+
+Project completion: TBD
--- frontend/controllers/videosToTranslateController.js
+++ frontend/controllers/videosToTranslateController.js
@@ -1,57 +1,56 @@
 app.controller('videosToTranslateController',['$scope','$http','$sce', function($scope, $http,$sce){
-    // $scope.videoToBeTranslated = "http://placehold.it/350x150"
-    $scope.videoToBeTranslatedArray = [];
-    var colors = [];
-	var tempUrl = 'http://localhost:3000/videos'
-	// $scope.imagePath = 'http://image.tmdb.org/t/p/w300/';
+   $scope.videoToBeTranslatedArray = [];
+   var colors = [];
+    var tempUrl = 'http://localhost:3000/videos'
+    // $scope.imagePath = 'http://image.tmdb.org/t/p/w300/';
 
-	$http({
-		method: "GET",
-		url: tempUrl
-	}).then(
-		function successFunction(videoData){
-			videoData.data.sort(function(a, b){
-			    if(a.familyName < b.familyName) return -1;
-			    if(a.familyName > b.familyName) return 1;
-			    return 0;
-			})				
+    $http({
+        method: "GET",
+        url: tempUrl
+    }).then(
+        function successFunction(videoData){
+            videoData.data.sort(function(a, b){
+                if(a.familyName < b.familyName) return -1;
+                if(a.familyName > b.familyName) return 1;
+                return 0;
+            })                
 
-			$scope.videoData = videoData
+            $scope.videoData = videoData
 
-			var tempFamilyName = '';
-			$scope.videoData.data.map((video, index)=>{
-				var myUrl = 'http://localhost:3000/videos/' + video.name
-        		video.name = $sce.trustAsResourceUrl(myUrl)
-				if(!index){
-					tempFamilyName = video.familyName
-				}else if(video.familyName.toUpperCase() == tempFamilyName.toUpperCase()){
-					video.familyName = ''
-				}else{
-					tempFamilyName = video.familyName
+            var tempFamilyName = '';
+            $scope.videoData.data.map((video, index)=>{
+                var myUrl = 'http://localhost:3000/videos/' + video.name
+               video.name = $sce.trustAsResourceUrl(myUrl)
+                if(!index){
+                    tempFamilyName = video.familyName
+                }else if(video.familyName.toUpperCase() == tempFamilyName.toUpperCase()){
+                    video.familyName = ''
+                }else{
+                    tempFamilyName = video.familyName
 
-				}
+                }
 
-				if(video.finished){
-					video.finished = 'Complete'
-					video.classStyle = 'video-complete'
-					video.checkItOutClass = ''
-				}else{
-					video.finished = 'Incomplete'
-					video.classStyle = 'video-incomplete'
-					video.checkItOutClass = 'displayNone'
-				}
-			})
-			console.log($scope.videoData.data[0].classStyle)
-			// $scope.videoToBeTranslatedArray.push(videoData.data)
-			// $scope.tempArray = []
+                if(video.finished){
+                    video.finished = 'Complete'
+                    video.classStyle = 'video-complete'
+                    video.checkItOutClass = ''
+                }else{
+                    video.finished = 'Incomplete'
+                    video.classStyle = 'video-incomplete'
+                    video.checkItOutClass = 'displayNone'
+                }
+            })
+            console.log($scope.videoData.data[0].classStyle)
+            // $scope.videoToBeTranslatedArray.push(videoData.data)
+            // $scope.tempArray = []
 
-			// videoData.data.map((eachVideo, index)=>{
-			// 	console.log(eachVideo)
-			// 	$scope.videoToBeTranslatedArray.push(eachVideo.name)
-			// })
-		},
-		function failedFunction(videoData){
-			// console.log(videoData)
-		}
-	)
+            // videoData.data.map((eachVideo, index)=>{
+            //     console.log(eachVideo)
+            //     $scope.videoToBeTranslatedArray.push(eachVideo.name)
+            // })
+        },
+        function failedFunction(videoData){
+            // console.log(videoData)
+        }
+    )
 }]);
