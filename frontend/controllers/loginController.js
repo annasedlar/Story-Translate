00e76420ca4f5872250956842fdb2e6c772d58db
Anna Sedlar <annasedlar@gmail.com>
2017-03-15 11:32:11 -0400

    login page -currently not working

---
+++ frontend/controllers/loginController.js
@@ -0,0 +1,19 @@
+app.controller('loginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+  
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,11 @@
-var app = angular.module('app', ['ui.router', 'ui.materialize']);
+var app = angular.module('app', ['ui.router', 'ui.materialize', 'ngCookies']);
 
+//declare modules
+angular.module('Authentication', []);
+angular.module('Home', []);
+angular.module('BasicHttpAuthExample', ['Authentication', 'Home','ngCookies']);
 
-app.config(['$stateProvider', '$urlRouterProvider', '$sceDelegateProvider', function($stateProvider, $urlRouterProvider, $sceDelegateProvider){
+app.config(['$stateProvider', '$urlRouterProvider', '$sceDelegateProvider', '$AuthenticationService' function($stateProvider, $urlRouterProvider, $sceDelegateProvider, AuthenticationService){
     // give name to state. in this case, it is 'firstMessage'
     // as a part of the stateProvider, you need to provide a new state by doing $stateProvider.state
     $stateProvider.state('home',{
@@ -11,10 +15,8 @@ app.config(['$stateProvider', '$urlRouterProvider', '$sceDelegateProvider', func
 
     })
     $stateProvider.state('videosToTranslate',{
-    
         // provide options for state in between the {}
         // url is specifying the route for the state
-
         url:'/videosToTranslate',
         // template:'<strong>This is the first message</strong>'
         templateUrl:'views/videosToTranslate.html',
@@ -41,6 +43,11 @@ app.config(['$stateProvider', '$urlRouterProvider', '$sceDelegateProvider', func
         templateUrl:'views/videoProduct.html',
         controller: 'videoProductController'
     })        
+    $stateProvider.state('login',{
+        url:'/login',
+        templateUrl: 'views/login.html',
+        controller: 'loginController'
+    })
     $urlRouterProvider.otherwise('/');
 }]);
 
@@ -50,4 +57,18 @@ app.filter('trustUrl',function($sce){
     }
 })
 
-
+app.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+  
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
