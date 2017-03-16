95170b2fa9f596e1dc276df1c7cec8c745a044f5
Anna Sedlar <annasedlar@gmail.com>
2017-03-16 11:58:46 -0400

    localhosts

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -1,6 +1,6 @@
 app.controller('translateVideoController',['$scope', '$location', '$http', '$sce', function($scope, $location, $http, $sce){
 	var paramsId = $location.$$path.slice(16)
-	var onLoadUrl = 'http://annasedlar.com:3003/transcript/' + paramsId
+	var onLoadUrl = 'http://localhost:3000/transcript/' + paramsId
 	$http({
     	method: "GET",
     	url: onLoadUrl
@@ -25,7 +25,7 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 		} 
   	)
 	$scope.videoToTranslateUrl = ''
-	var tempUrl = 'http://annasedlar.com:3003/videos'
+	var tempUrl = 'http://localhost:3000/videos'
 	$http({
     	method: "GET",
     	url: tempUrl
@@ -39,7 +39,7 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
         			$scope.familyName = eachVideo.familyName
 			        // console.log(eachVideo.path.slice(7))
 			        console.log(tempVideoPath)
-        			var myUrl = 'http://annasedlar.com:3003/' + tempVideoPath
+        			var myUrl = 'http://localhost:3000/' + tempVideoPath
         			$scope.pleasWork = $sce.trustAsResourceUrl(myUrl)
        			}
       		})
@@ -145,7 +145,7 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 		}
   	    $http({
 			method:'POST',
-      		url: 'http://annasedlar.com:3003/changeFamilyName/',
+      		url: 'http://localhost:3000/changeFamilyName/',
       		data: tempDataToSend
     	}).then(
 	      	function successFunction(data){
@@ -159,7 +159,7 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 
   	// send transcript to the backend
 	$scope.saveForm = function(){
-		var transcriptUrl = 'http://annasedlar.com:3003/transcript/' + $location.$$path.slice(16)
+		var transcriptUrl = 'http://localhost:3000/transcript/' + $location.$$path.slice(16)
 		$http({
 			method:'POST',
       		url: transcriptUrl,
@@ -177,14 +177,14 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 
 	$scope.submitForm = function(){
 		console.log($scope.entireTranscript)
-		var transcriptUrl = 'http://annasedlar.com:3003/transcript/' + $location.$$path.slice(16);
+		var transcriptUrl = 'http://localhost:3000/transcript/' + $location.$$path.slice(16);
 		$http({
 			method:'POST',
       		url: transcriptUrl,
       		data: $scope.entireTranscript
     	}).then(
       		function successFunction(data){			
-				var finishedUrl = 'http://annasedlar.com:3003/finished/' + $location.$$path.slice(16);
+				var finishedUrl = 'http://localhost:3000/finished/' + $location.$$path.slice(16);
 		// 		console.log(finishedUrl)
 				var dataArray = [1]
 				$http({
@@ -220,7 +220,7 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 
 	$scope.deleteTranscript = function(index){
 		$scope.entireTranscript.splice(index, 1)
-		var transcriptUrl = 'http://annasedlar.com:3003/transcript/' + $location.$$path.slice(16)
+		var transcriptUrl = 'http://localhost:3000/transcript/' + $location.$$path.slice(16)
 		$http({
 			method:'POST',
       		url: transcriptUrl,
