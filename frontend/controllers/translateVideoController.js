a6b38c14b495db89c5542486e7dd2cd2f624c8ec
Anna Sedlar <annasedlar@gmail.com>
2017-03-17 14:01:07 -0400

    minutes:seconds on translate video page

--- frontend/controllers/translateVideoController.js
+++ frontend/controllers/translateVideoController.js
@@ -63,14 +63,30 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 			var month = (date.getMonth()+1).toString(); 
 			var year = (date.getUTCFullYear()).toString(); 
 
-			var seconds = date.getSeconds();
-			var minutes = date.getMinutes(); 
-			var timeFrame = minutes+':'+seconds
+			var startMinutes = Math.floor($scope.startTime / 60);
+			if(startMinutes < 10){
+				startMinutes = '0'+ startMinutes;
+			}
+			var startSeconds = Math.floor($scope.startTime - startMinutes * 60);
+			if(startSeconds < 10){
+				startSeconds = "0"+ startSeconds;
+			}
+			var endMinutes = Math.floor($scope.endTime / 60);
+			if(endMinutes < 10){
+				endMinutes =  "0"+endMinutes;
+			}
+			var endSeconds = Math.floor($scope.endTime - endMinutes * 60);
+			if(endSeconds < 10){
+				endSeconds = "0"+ endSeconds;
+			}
+
 	        $scope.entireTranscript[$scope.clickedTranscriptIndex] = {
 	      		startTime: $scope.startTime,
 	        	endTime: $scope.endTime,
 	          	transcript: $scope.transcript,
-	          	postedTime: month+'/'+day+'/'+year
+	          	postedTime: month+'/'+day+'/'+year,
+	          	startMinsSecs: startMinutes+":"+startSeconds,
+				endMinsSecs: endMinutes+":"+endSeconds
 	          	
 	        }
 	        $scope.editOrAddButton = 'Add to Transcript'
@@ -124,15 +140,32 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 			var day = (date.getUTCDate()).toString();
 			var month = (date.getMonth()+1).toString(); 
 			var year = (date.getUTCFullYear()).toString(); 
-			var seconds = date.getSeconds();
-			var minutes = date.getMinutes(); 
-			var timeFrame = minutes+':'+seconds
+
+			var startMinutes = Math.floor($scope.startTime / 60);
+			if(startMinutes < 10){
+				startMinutes = '0'+ startMinutes;
+			}
+			var startSeconds = Math.floor($scope.startTime - startMinutes * 60);
+			if(startSeconds < 10){
+				startSeconds = "0"+ startSeconds;
+			}
+			var endMinutes = Math.floor($scope.endTime / 60);
+			if(endMinutes < 10){
+				endMinutes =  "0"+endMinutes;
+			}
+			var endSeconds = Math.floor($scope.endTime - endMinutes * 60);
+			if(endSeconds < 10){
+				endSeconds = "0"+ endSeconds;
+			}
+
 	        if(index == -1){
 	        	$scope.entireTranscript.push({
 					startTime: $scope.startTime,
 					endTime: $scope.endTime,
 					transcript: $scope.transcript,
-					postedTime: month+'/'+day+'/'+year
+					postedTime: month+'/'+day+'/'+year,
+					startMinsSecs: startMinutes+":"+startSeconds,
+					endMinsSecs: endMinutes+":"+endSeconds
 				})    
 			}
 			$scope.clickedTranscriptIndex = -1
@@ -142,7 +175,17 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
   	$scope.startTimeFunc = function(){
     	var theVid = document.getElementById("theVid")
     	$scope.startTime = theVid.currentTime.toFixed(0)
-    	// theVid.currentTime = 5
+    		var startMinutes = Math.floor($scope.startTime / 60);
+			if(startMinutes < 10){
+				startMinutes = '0'+ startMinutes;
+			}
+			var startSeconds = Math.floor($scope.startTime - startMinutes * 60);
+			if(startSeconds < 10){
+				startSeconds = "0"+ startSeconds;
+			}
+			$scope.startMins = startMinutes
+			$scope.startSeconds = startSeconds
+			$scope.startTimes = startMinutes+":"+startSeconds
     	theVid.play(); 
   	}	 
   	$scope.endTimeFunc = function(){
@@ -151,6 +194,17 @@ app.controller('translateVideoController',['$scope', '$location', '$http', '$sce
 		if(endTempTime > $scope.startTime){
 			$scope.endTime = endTempTime
 		}
+		var endMinutes = Math.floor($scope.endTime / 60);
+		if(endMinutes < 10){
+			endMinutes =  "0"+endMinutes;
+		}
+		var endSeconds = Math.floor($scope.endTime - endMinutes * 60);
+		if(endSeconds < 10){
+			endSeconds = "0"+ endSeconds;
+		}
+		$scope.endMins = endMinutes
+		$scope.endSecs = endSeconds
+		$scope.endTimes = endMinutes+":"+endSeconds
 		theVid.pause(); 
   	}
   	$scope.rewind = function(){
