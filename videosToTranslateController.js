9c05f1f09830cb560bf1f1313969f123b9f92d08
Anna Sedlar <annasedlar@gmail.com>
2017-03-22 09:49:44 -0400

    videos to Translate list add delete video functionality and margin-bottom to each class

--- videosToTranslateController.js
+++ videosToTranslateController.js
@@ -1,3 +1,40 @@
+<a href='http://www.annasedlar.com/Story-Translate/frontend/#/'><div class='go-home3'><span class='glyphicon glyphicon-home'></span></div></a>
+
+<div class="final-product-header"><h1>Click a video to translate or view</h1></div>
+<div>
+<!-- style="position:fixed; margin-top:20%; margin-left:4%; width:200px; z-index:6; background-color:lightgrey; height:70px; border-radius: 50%"> -->
+{{deletedMessage}}</div>
+    <div ng-repeat="video in videoData.data" class='translate-background'> 
+        <div ng-class='video.skipClass' class='col-xs-12 skip-placeholder '>
+            &nbsp;
+        </div>
+        <div ng-class='video.skipClass' class='col-xs-12 video-data-1'>{{video.familyName}}</div>
+        <div class='col-lg-4 col-sm-6 col-xs-12 each-video-row'>
+            <div class='card-panel card-class'>
+                <button class='delete-button' ng-click='deleteVideo(video)'>x</button>
+                <div class='each-video-description video-data-2' '>
+                    <a ng-href='#/translateVideo/{{video.token}}'>
+                        <video ng-src="{{video.name}}"></video>
+                        <div class='translate-letter'>TRANSLATE</div>
+                    </a>
+                </div>
+                <!-- <div>{{deletedMessage}}</div> -->
+                <div class='each-video-description video-data-3' ng-class='video.classStyle'>
+                    {{video.finished}}
+                    <a ng-class='video.checkItOutClass' ng-href='#/videoProduct/{{video.token}}'>
+                        <button class='btn teal accent-4 watch-button'>Complete</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
 app.controller('videosToTranslateController',['$scope','$http','$sce', function($scope, $http,$sce){
    $scope.videoToBeTranslatedArray = [];
    var colors = [];
